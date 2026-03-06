fe7c8610b9bebbff25b09748aef9691c6eb61537
M. Sofyan Irwanto <sofyan@fleetify.id>
2026-03-06 14:44:03 +0700

    initial commit

---
+++ services/hr-service/cmd/server/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v3"
+
+	"coraflow-erp-api/shared/config"
+)
+
+func main() {
+
+	cfg := config.Load()
+
+	app := fiber.New()
+
+	app.Get("/health", func(c fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"service": "api-gateway",
+			"env":     cfg.AppEnv,
+		})
+	})
+
+	log.Fatal(app.Listen(fmt.Sprintf(":%s", cfg.HRServicePort)))
+}
---
+++ shared/config/config.go
@@ -0,0 +1,70 @@
+package config
+
+import (
+	"log"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppEnv string
+
+	ApiGatewayPort  string
+	UserServicePort string
+	HRServicePort   string
+
+	PostgresHost     string
+	PostgresPort     string
+	PostgresUser     string
+	PostgresPassword string
+
+	UserDB string
+	HRDB   string
+
+	RedisHost string
+	RedisPort string
+
+	RabbitMQHost string
+	RabbitMQPort string
+
+	JWTSecret string
+}
+
+func Load() *Config {
+
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	config := &Config{
+		AppEnv: viper.GetString("APP_ENV"),
+
+		ApiGatewayPort:  viper.GetString("API_GATEWAY_PORT"),
+		UserServicePort: viper.GetString("USER_SERVICE_PORT"),
+		HRServicePort:   viper.GetString("HR_SERVICE_PORT"),
+
+		PostgresHost:     viper.GetString("POSTGRES_HOST"),
+		PostgresPort:     viper.GetString("POSTGRES_PORT"),
+		PostgresUser:     viper.GetString("POSTGRES_USER"),
+		PostgresPassword: viper.GetString("POSTGRES_PASSWORD"),
+
+		UserDB: viper.GetString("USER_DB"),
+		HRDB:   viper.GetString("HR_DB"),
+
+		RedisHost: viper.GetString("REDIS_HOST"),
+		RedisPort: viper.GetString("REDIS_PORT"),
+
+		RabbitMQHost: viper.GetString("RABBITMQ_HOST"),
+		RabbitMQPort: viper.GetString("RABBITMQ_PORT"),
+
+		JWTSecret: viper.GetString("JWT_SECRET"),
+	}
+
+	return config
+}
